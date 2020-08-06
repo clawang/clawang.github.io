7d367f1466f20b3951ff73d16fe991cba6603c36
Claire Wang <clairewang@Claires-MacBook-Pro-10.local>
2020-08-06 12:34:11 -0400

    Updates

---
+++ precache-manifest.cd2a6985c961c5ab47024d1972602ad1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f95207625b8644145d88c9df1051b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3536edbdca0192259e50",
+    "url": "/static/css/main.2521ee07.chunk.css"
+  },
+  {
+    "revision": "8c0949bce3af27053772",
+    "url": "/static/js/2.df0fd747.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.df0fd747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3536edbdca0192259e50",
+    "url": "/static/js/main.bb65b3ff.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "40677108180707590c57467f014f697a",
+    "url": "/static/media/bluestripes.40677108.jpg"
+  },
+  {
+    "revision": "8c291894e45aefedbddba1fdd6840936",
+    "url": "/static/media/claire.8c291894.png"
+  },
+  {
+    "revision": "c54c1027d09bc3a333df8254c677543a",
+    "url": "/static/media/linkedin.c54c1027.svg"
+  },
+  {
+    "revision": "4ac867a2a5dc317b5c97ff3059af465d",
+    "url": "/static/media/login.4ac867a2.png"
+  },
+  {
+    "revision": "9d2161705e27c3adfa0b674bc138b211",
+    "url": "/static/media/mail.9d216170.svg"
+  },
+  {
+    "revision": "55e435a871b1748f66541069e822c089",
+    "url": "/static/media/paris.55e435a8.jpg"
+  },
+  {
+    "revision": "858f360f83ea8724b2710d31ac034016",
+    "url": "/static/media/portfolio.858f360f.png"
+  },
+  {
+    "revision": "921c406acf4fdb88b06b356533f0c3d8",
+    "url": "/static/media/tot.921c406a.png"
+  },
+  {
+    "revision": "a4bb57ec4b37a1e2662145ab6c9c0361",
+    "url": "/static/media/tulum.a4bb57ec.jpg"
+  },
+  {
+    "revision": "36e6d00f79490bf1a4f499e468c2f3f7",
+    "url": "/static/media/virtualnyc.36e6d00f.png"
+  },
+  {
+    "revision": "50160ea6494d9262a1c4fbfe9c0e4097",
+    "url": "/static/media/westvillage.50160ea6.jpg"
+  }
+]);
